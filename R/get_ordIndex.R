f76c9fdf8835cd875b97d30420224f29288123c8
lhzmaki <kpcsfever@gmail.com>
2016-08-22 16:50:04 +0800

    code & data

--- R/get_ordIndex.R
+++ R/get_ordIndex.R
@@ -1,8 +1,8 @@
 source("bestEnsembleComplexTSP.r")
 
-get_ordIndex <- function(test_exp)
+get_ordIndex <- function(test_exp, threadnum)
 {
-  result <- bestEnsembleComplexTSP(test_exp = test_exp, TSPFold = 2, beginNum = 10, endNum = dim(test_exp)[1], threads = 20)
+  result <- bestEnsembleComplexTSP(test_exp = test_exp, TSPFold = 2, beginNum = 10, endNum = dim(test_exp)[1], threads = threadnum)
   ord <- result$ensembleResultLst[dim(result$ensembleResultLst)[1], ]
   ordIndex <- order(ord)
   return(ordIndex)
