918e852602fa6d1168c5efffe7add145a645f88c
louhuazhe <707553427@qq.com>
2016-09-16 22:15:32 +0800

    .

--- R/get_ordIndex.R
+++ R/get_ordIndex.R
@@ -5,9 +5,9 @@
 
 source("bestEnsembleComplexTSP.r")
 
-get_ordIndex <- function(test_exp, threadnum)
+get_ordIndex <- function(test_exp, threadnum, cls_num)
 {
-  result <- bestEnsembleComplexTSP(test_exp = test_exp, TSPFold = 2, beginNum = 10, endNum = dim(test_exp)[1], threads = threadnum)
+  result <- bestEnsembleComplexTSP(test_exp = test_exp, TSPFold = 2, beginNum = 10, endNum = cls_num, threads = threadnum)
   ord <- result$ensembleResultLst[dim(result$ensembleResultLst)[1], ]
   ordIndex <- order(ord)
   return(ordIndex)
